ca2ed8a5a02970914738c48d0ffdb94be64ee812
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-23 10:45:49 +0530

    rendering the inner dynamic values and strings  using handleStart function

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
+useState
 export default function TimerChallenge({title, targetTime}){
+    const [timerExpired,setTimerExpired] = useState(false);
+    const [timerStarted,setTimerStarted] = useState(false);
+    useState(false);
+    function handleStart(){
+        setTimeout(()=>{
+            setTimerExpired(true);
+        }, targetTime*1000);
+        setTimerStarted(true);
+    }
+
 
     return(
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You Lost !</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime >1 ? 's':''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop':'Start'} Challenge
                 </button>
             </p>
             <p className="">
-                Time is running... / Timer inactive
+                {timerStarted ? 'Time is running...' : 'Timer inactive'} /
             </p>
         </section>
 
